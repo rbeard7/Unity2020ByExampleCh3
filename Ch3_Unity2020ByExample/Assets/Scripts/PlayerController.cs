830cef1080f92d47759cc76180c78513471a08bf
rbeard7 <84631913+rbeard7@users.noreply.github.com>
2021-09-18 14:23:44 -0700

    Initial Push

---
+++ Ch3_Unity2020ByExample/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool MouseLook = true;
+    public string HorzAxis = "Horizontal";
+    public string VertAxis = "Vertical";
+    public string FireAxis = "Fire1";
+    public float MaxSpeed = 5f;
+
+    private Rigidbody ThisBody = null;
+
+
+    private void Awake()
+    {
+        ThisBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float Horz = Input.GetAxis("HorzAxis");
+        float Vert = Input.GetAxis("VertAxis");
+        Vector3 MoveDirection = new Vector3(Horz, 0.0f, Vert);
+        ThisBody.AddForce(MoveDirection.normalized * MaxSpeed);
+
+        ThisBody.velocity = new Vector3
+            (Mathf.Clamp(ThisBody.velocity.x, -MaxSpeed, MaxSpeed),
+             Mathf.Clamp(ThisBody.velocity.y, -MaxSpeed, MaxSpeed),
+             Mathf.Clamp(ThisBody.velocity.z, -MaxSpeed, MaxSpeed));
+
+        if(MouseLook)
+        {
+            Vector3 MousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y, 0.0f));
+            MousePosWorld = new Vector3(MousePosWorld.x, 0.0f, MousePosWorld.z);
+            Vector3 lookDirection = MousePosWorld - transform.position;
+
+            transform.localRotation = Quaternion.LookRotation(lookDirection.normalized, Vector3.up);
+        }
+    }
+}
